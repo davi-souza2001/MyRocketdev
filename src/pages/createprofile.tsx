c8855f9bbc8607dea777c802df53d772419686d5
davi-souza2001 <davisouza2001dv@gmail.com>
2021-09-28 09:25:48 -0300

    set name in login

--- src/pages/createprofile.tsx
+++ src/pages/createprofile.tsx
@@ -4,10 +4,10 @@ import styles from "../styles/CreateProfile.module.css";
 import useAuth from "../data/hook/useAuth";
 
 interface CreateProfileProps {
-    
+
 }
 
-export default function CreateProfile(Props: CreateProfileProps){
+export default function CreateProfile(Props: CreateProfileProps) {
     const { user } = useAuth();
 
     const [name, setName] = useState("");
@@ -20,39 +20,34 @@ export default function CreateProfile(Props: CreateProfileProps){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function navigateToNextRoom(e: any) {
-        e.preventDefault();
+    async function setDatas(){
+        const nameUser = await user?.name;
+        setName(nameUser);
         const datas = {
-            name,
-            userName,
-            linkedin,
-            github,
-            instagram,
-            youtube,
-            description,
-            email,
-            password
-        }
-
+            nameUser
+        };
         console.log(datas);
     }
+
+    setDatas();
+
 
     return (
         <div className={styles.contentGeral}>
             <div className={styles.contentForms}>
                 <form>
-                    <h2>{user ? user.name : "Ola"}</h2>
-                    <input type="text" placeholder="Digite seu nome publico" onChange={(e) => setName(e.target.value)}/>
+                    <h2>Cadastro</h2>
+                    <input type="text" placeholder="Digite seu nome publico" value={name} />
                     <input type="text" placeholder="Digite seu @ no qual as pessoas vão procurar você" onChange={(e) => setUserName(e.target.value)} />
                     <input type="text" placeholder="Linkedin" onChange={(e) => setLinkedin(e.target.value)} />
                     <input type="text" placeholder="Github" onChange={(e) => setGithub(e.target.value)} />
                     <input type="text" placeholder="Instagrm" onChange={(e) => setInstagram(e.target.value)} />
-                    <input type="text" placeholder="Youtube" onChange={(e) => setYoutube(e.target.value)}/>
+                    <input type="text" placeholder="Youtube" onChange={(e) => setYoutube(e.target.value)} />
                     <input type="text" placeholder="Adicione uma descrição sobre você" onChange={(e) => setDescription(e.target.value)} />
-                    <input type="text" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)}/>
-                    <input type="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="text" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)} />
                     <p>Ao se cadastrar você concorda com os termos de uso da rede social. Deseja ler ? então clique <a href="https://github.com/davi-souza2001/Myrocket" target="_blank" rel="noreferrer">aqui</a> </p>
-                    <button type="submit" className={styles.buttonSubmit} onClick={navigateToNextRoom}>Cadastrar</button>
+                    <button type="submit" className={styles.buttonSubmit}>Cadastrar</button>
                 </form>
             </div>
         </div>
