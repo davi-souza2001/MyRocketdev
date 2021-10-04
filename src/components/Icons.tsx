19d30ec9c1a5bf16f3bdd3340162f17852f91653
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-04 10:56:21 -0300

    implements icons in leftbar

---
+++ src/components/Icons.tsx
@@ -0,0 +1,17 @@
+export const IconHome = (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+    </svg>
+);
+
+export const IconSearch = (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+    </svg>
+)
+
+export const IconProfile = (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+    </svg>
+)
--- src/components/Leftbar.tsx
+++ src/components/Leftbar.tsx
@@ -4,6 +4,9 @@ import Astro from "../assets/img/austrotwo.jpg";
 import route from "next/router";
 import Image from "next/image";
 
+import { IconHome } from "./Icons";
+import { IconSearch } from "./Icons";
+import { IconProfile } from "./Icons";
 import styles from "../styles/Leftbar.module.css";
 
 interface LeftbarProps {
@@ -30,9 +33,9 @@ export default function Leftbar(props: LeftbarProps) {
                     <div className={styles.title}><h1>MyRocket</h1></div>
                 </div>
                 <div className={styles.iconstab}>
-                    <div className={styles.home} onClick={navigateToHomeRoom}><h3>Home</h3></div>
-                    <div className={styles.search}><h3>Search</h3></div>
-                    <div className={styles.profile} onClick={navigateToProfileRoom}><h3>Profile</h3></div>
+                    <div className={styles.home} onClick={navigateToHomeRoom}><div className={styles.icon}>{IconHome}</div><h3>Home</h3></div>
+                    <div className={styles.search}><div className={styles.icon}>{IconSearch}</div><h3>Search</h3></div>
+                    <div className={styles.profile} onClick={navigateToProfileRoom}><div className={styles.icon}>{IconProfile}</div><h3>Profile</h3></div>
                 </div>
                 <div className={styles.contatctab}>
                     <div className={styles.user} onClick={user ? logout : loginGoogle }>
