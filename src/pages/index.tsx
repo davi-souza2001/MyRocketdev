f8320ef53d3a40a464fdcebe49906616f1b7aad3
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-12 17:42:37 -0300

    starting main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Topbar from "../components/Topbar";
-import MainContent from "../components/ContentProfiles";
+
+import styles from "../styles/Main.module.css";
 
 interface MainProps {
     
@@ -9,6 +10,23 @@ export default function Main(props: MainProps){
     return (
         <div>
             <Topbar/>
+            <div className={styles.contentCommunity}>
+                <h2>Comunidades</h2>
+            </div>
+            <div className={styles.contentCommunityOptions}>
+                <div className={styles.contentOption}>
+                    <h2>Front-End</h2>
+                    <h4>Aqui você pode ficar por dentro do que nossos astronautas estão discutindo sobre fron-end</h4>
+                </div>
+                <div className={styles.contentOption}>
+                    <h2>Back-End</h2>
+                    <h4>Aqui você pode ficar por dentro do que nossos astronautas estão discutindo sobre fron-end</h4>
+                </div>
+                <div className={styles.contentOption}>
+                    <h2>Mobile</h2>
+                    <h4>Aqui você pode ficar por dentro do que nossos astronautas estão discutindo sobre fron-end</h4>
+                </div>
+            </div>
         </div>
     )
 }
