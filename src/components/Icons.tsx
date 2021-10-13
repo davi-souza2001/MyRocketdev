2e659bc2f30a04cd919080ab3b9cc6499fd0b990
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-13 09:41:59 -0300

    up

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -15,3 +15,9 @@ export const IconProfile = (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
     </svg>
 )
+
+export const IconUsersList = (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+    </svg>
+)
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { IconHome } from "./Icons";
 import { IconSearch } from "./Icons";
 import { IconProfile } from "./Icons";
+import { IconUsersList } from "./Icons";
 import styles from "../styles/Topbar.module.css";
 
 interface TopbarProps {
@@ -29,6 +30,7 @@ export default function Topbar(props: TopbarProps) {
                 </div>
                 <div className={styles.iconstab}>
                     <div className={styles.home} onClick={() => route.push("/")}><div className={styles.iconstabIcon}>{IconHome}</div></div>
+                    <div className={styles.home} onClick={() => route.push("/")}><div className={styles.iconstabIcon}>{IconUsersList}</div></div>
                     <div className={styles.profile} onClick={() => route.push("/mainprofile")}><div className={styles.iconstabIcon}>{IconProfile}</div></div>
                 </div>
                 <div className={styles.usertab}>
