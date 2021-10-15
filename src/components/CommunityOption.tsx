e92eb6b8edf75bba157b3067419fa8726ca82b8b
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-15 09:17:55 -0300

    implements router

--- src/components/CommunityOption.tsx
+++ src/components/CommunityOption.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import Image from "next/image";
+import route from "next/router";
 
 import styles from "../styles/Main.module.css";
 
@@ -7,11 +8,12 @@ interface CommunityOption {
     image: any;
     tec : any;
     area: any;
+    routeCommunity: any;
 }
 
 export default function CommunityOption(props: CommunityOption) {
     return (
-        <div className={styles.contentOption}>
+        <div className={styles.contentOption} onClick={() => route.push(`/com/${props.routeCommunity}`)}>
             <div>
                 <Image alt="astro" src={props.image} height={100} width={200} />
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,27 +37,27 @@ export default function Main() {
             <div className={styles.contentCommunityOptions}>
                 <h2>Front-End</h2>
                 <div className={styles.frameworks}>
-                    <CommunityOption image={reactImg} tec="React.Js" area="Front-End"/>
-                    <CommunityOption image={angularImg} tec="Angular.js" area="Front-End"/>
-                    <CommunityOption image={vueImg} tec="Vue.js" area="Front-End"/>
-                    <CommunityOption image={nextImg} tec="Next.js" area="Front-End"/>
-                    <CommunityOption image={emberImg} tec="Ember" area="Front-End"/>
+                    <CommunityOption image={reactImg} tec="React.Js" area="Front-End" routeCommunity="React"/>
+                    <CommunityOption image={angularImg} tec="Angular.js" area="Front-End" routeCommunity="Angular"/>
+                    <CommunityOption image={vueImg} tec="Vue.js" area="Front-End" routeCommunity="Vue"/>
+                    <CommunityOption image={nextImg} tec="Next.js" area="Front-End" routeCommunity="Next"/>
+                    <CommunityOption image={emberImg} tec="Ember" area="Front-End" routeCommunity="Ember"/>
                 </div>
                 <h2>Back-End</h2>
                 <div className={styles.frameworks}>
-                    <CommunityOption image={nodeImg} tec="Node.js" area="Back-End"/>
-                    <CommunityOption image={mongoImg} tec="Mongo" area="Back-End"/>
-                    <CommunityOption image={sqlImg} tec="Sql/MySql" area="Back-End"/>
-                    <CommunityOption image={firebaseImg} tec="Firebase" area="Back-End"/>
-                    <CommunityOption image={csharpImg} tec="C#" area="Back-End"/>
-                    <CommunityOption image={javaImg} tec="Java" area="Back-End"/>
+                    <CommunityOption image={nodeImg} tec="Node.js" area="Back-End" routeCommunity="Node"/>
+                    <CommunityOption image={mongoImg} tec="Mongo" area="Back-End" routeCommunity="Mongo"/>
+                    <CommunityOption image={sqlImg} tec="Sql/MySql" area="Back-End" routeCommunity="Sql"/>
+                    <CommunityOption image={firebaseImg} tec="Firebase" area="Back-End" routeCommunity="Firebase"/>
+                    <CommunityOption image={csharpImg} tec="C#" area="Back-End" routeCommunity="CSharp"/>
+                    <CommunityOption image={javaImg} tec="Java" area="Back-End" routeCommunity="Java"/>
                 </div>
                 <h2>Mobile</h2>
                 <div className={styles.frameworks}>
-                <CommunityOption image={reactImg} tec="React-Native" area="Mobile"/>
-                <CommunityOption image={flutterImg} tec="Flutter" area="Mobile"/>
-                <CommunityOption image={swiftImg} tec="Swift" area="Mobile"/>
-                <CommunityOption image={ionicImg} tec="Ionic" area="Mobile"/>
+                <CommunityOption image={reactImg} tec="React-Native" area="Mobile" routeCommunity="React-Native"/>
+                <CommunityOption image={flutterImg} tec="Flutter" area="Mobile" routeCommunity="Flutter"/>
+                <CommunityOption image={swiftImg} tec="Swift" area="Mobile" routeCommunity="Swift"/>
+                <CommunityOption image={ionicImg} tec="Ionic" area="Mobile" routeCommunity="Ionic"/>
                 </div>
             </div>
         </>
