5d80fee5cc0a73871a1acad74b9412621715cb34
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-26 14:40:34 -0300

    implements members list

--- src/pages/com/[idcom].tsx
+++ src/pages/com/[idcom].tsx
@@ -9,32 +9,37 @@ import astroMyRocket from "../../assets/img/astrounauta.svg";
 
 import useAuth from "../../data/hook/useAuth";
 import firebase from "../../firebase/config";
+import useProfile from "../../data/hook/useProfile";
 
 import styles from "../../styles/Com.module.css"
 import CommunityMembers from "../../components/CommunityMembers";
 
 interface idCommunitie {
-    
+
 }
 
-export default function Com(props: idCommunitie){
+export default function Com(props: idCommunitie) {
     const router = useRouter();
     const idcom = router.query.idcom;
     const [publisList, setPublisListList] = useState([]);
 
     const { user } = useAuth();
+    const { profileList } = useProfile();
+
+
+
 
     useEffect(() => {
         const todoRef = firebase.database().ref(idcom?.toString());
         todoRef.on('value', (snapshot) => {
-          const todos = snapshot.val();
-          const todoList = [];
-          for (let id in todos) {
-            todoList.push({ id, ...todos[id] });
-          }
-          setPublisListList(todoList);
+            const todos = snapshot.val();
+            const todoList = [];
+            for (let id in todos) {
+                todoList.push({ id, ...todos[id] });
+            }
+            setPublisListList(todoList);
         })
-      }, []);
+    }, []);
 
     return (
         <div className={styles.content}>
@@ -48,18 +53,28 @@ export default function Com(props: idCommunitie){
                 </div>
             </div>
             <div className={styles.divRow}>
-                <div className={styles.row}></div><Image src={astroMyRocket} height={150} width={150} alt="Astro"/><div className={styles.row}></div>
+                <div className={styles.row}></div><Image src={astroMyRocket} height={150} width={150} alt="Astro" /><div className={styles.row}></div>
             </div>
             <div className={styles.contentMembersAndAddPost}>
                 <CommunityMembers linkComuList={idcom} showAdmin>
                     <div className={styles.contentMembers}>
-                            
+                        {profileList.map((prof) => {
+                            if (prof.firstComum == idcom || prof.secondComum == idcom || prof.thirdComum == idcom) {
+                                const nameOutSpace = prof.name.split(" ");
+                                return(
+                                    <div className={styles.contentMembersIntoComum}>
+                                        <Image src={prof.image} width={30} height={30}/>
+                                        <p key={prof.name}>{prof.name}</p> 
+                                    </div>
+                                )
+                            }
+                        })}
                     </div>
                 </CommunityMembers>
-                <AddPost linkComu={idcom} name={user ? user?.name : "Faça login para fazer parte da comunidade"}/>
-                <CommunityMembers linkComuList={idcom} showAdmin={false}/>
+                <AddPost linkComu={idcom} name={user ? user?.name : "Faça login para fazer parte da comunidade"} />
+                <CommunityMembers linkComuList={idcom} showAdmin={false} />
             </div>
-            <ListPublis linkComuList={idcom}/>
+            <ListPublis linkComuList={idcom} />
         </div>
     )
 }
