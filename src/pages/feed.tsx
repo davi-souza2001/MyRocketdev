e659add790310aa310f91e114fa8add576d2c263
davi-souza2001 <davisouza2001dv@gmail.com>
2021-11-11 20:38:51 -0300

    login out profile, page feed complete

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useState } from "react";
+import route from "next/router";
 
 import Topbar from "../components/Topbar";
 import ListPublis from "../components/ListPublis";
 import Image from "next/image";
 
 import useAuth from "../data/hook/useAuth";
-import firebase from "../firebase/config";
 
 import styles from "../styles/feedmain.module.css";
 
 import astroAlone from "../assets/img/astronauta_sozinho.svg";
 import useProfile from "../data/hook/useProfile";
 
-interface MainPage {
-
-}
-
-export default function MainPage(props: MainPage) {
+export default function MainPage() {
 
     const { user } = useAuth();
     const { profileList } = useProfile();
@@ -26,6 +22,8 @@ export default function MainPage(props: MainPage) {
     const [showfirst, setShowFirst] = useState(true);
     const [showsecond, setShowSecond] = useState(false);
     const [showthird, setShowThird] = useState(false);
+    
+    const [checkEmailProfile, setCheckEmailProfile] = useState(false);
 
     useEffect(() => {
         const getUserCommuns = profileList.map((prof) => {
@@ -37,16 +35,27 @@ export default function MainPage(props: MainPage) {
         })
     }, [user, profileList]);
 
-    function showFeedFirst(){
+    useEffect(() => {
+        const checkIfExistsemail = profileList?.map((prof) => {
+            if (user) {
+                if (prof.email == user?.email) {
+                    setCheckEmailProfile(true)
+                }
+            }
+        })
+    }, [user, profileList]);
+
+
+    function showFeedFirst() {
         setShowFirst(true);
         setShowSecond(false);
         setShowThird(false);
         document.getElementById("box1").style.backgroundColor = "#1613B2";
         document.getElementById("box2").style.backgroundColor = "#3936EB";
         document.getElementById("box3").style.backgroundColor = "#3936EB";
-    }
+    };
 
-    function showFeedSecond(){
+    function showFeedSecond() {
         setShowFirst(false);
         setShowSecond(true);
         setShowThird(false);
@@ -55,7 +64,7 @@ export default function MainPage(props: MainPage) {
         document.getElementById("box3").style.backgroundColor = "#3936EB";
     }
 
-    function showFeedThird(){
+    function showFeedThird() {
         setShowFirst(false);
         setShowSecond(false);
         setShowThird(true);
@@ -89,29 +98,31 @@ export default function MainPage(props: MainPage) {
                                 <div className={styles.feed}>
                                     <ListPublis linkComuList={first} />
                                 </div>
-                            ): false}
+                            ) : false}
                             {showsecond ? (
                                 <div className={styles.feed}>
                                     <ListPublis linkComuList={second} />
                                 </div>
-                            ): false}
+                            ) : false}
                             {showthird ? (
                                 <div className={styles.feed}>
                                     <ListPublis linkComuList={third} />
                                 </div>
-                            ): false}
+                            ) : false}
                         </div>
                     </>
-                ) : 
-                (
-                <>
-                <div className={styles.notLogged}>
-                    <p>Parece que você ainda não fez <strong>login</strong></p>
-                    <p>ou não entrou em alguma <strong>comunidade</strong></p>
-                    <Image src={astroAlone} height={300} width={300} alt="astro" />
-                </div>
-                </>
-                )}
+                ) :
+                    (
+                        <>
+                            <div className={styles.notLogged}>
+                                <p>Parece que você ainda não fez <strong>login</strong></p>
+                                <p>ou não entrou em alguma <strong>comunidade</strong></p>
+                                {user && checkEmailProfile ? (<p>Clique aqui para completar seu <strong style={{ cursor: "pointer" }} onClick={() => route.push("/mainprofile")}>perfil</strong></p>) : false}
+                                {user && !checkEmailProfile ? (<p>Parece que você não completou seu perfil, faça novamente seu login</p>) : false}
+                                <Image src={astroAlone} height={300} width={300} alt="astro" />
+                            </div>
+                        </>
+                    )}
             </div>
         </>
     )
